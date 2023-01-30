675a2c22caad072e8c9c56863db879091a5161e2
Hone <244573246@qq.com>
2023-01-31 00:27:20 +0800

    初始化SpringBoot Maven环境

---
+++ src/main/java/com/heyi/hygenerator/HyGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.heyi.hygenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyGeneratorApplication.class, args);
+	}
+
+}
